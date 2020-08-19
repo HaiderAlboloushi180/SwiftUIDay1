bf05181c615d3babaae079081caaaca16e905a4a
HaiderAlboloushi180 <halbolou05@gmail.com>
2020-08-19 19:47:08 +0300

    Still Working

--- SwiftUIProject/SwiftUIProject/AssignmentCalculator.swift
+++ SwiftUIProject/SwiftUIProject/AssignmentCalculator.swift
@@ -2,7 +2,7 @@
 //  AssignmentCalculator.swift
 //  SwiftUIProject
 //
-//  Created by Fatimah Alhashem on 8/19/20.
+//  Created by Haider Alboloushi on 8/19/20.
 //  Copyright © 2020 Haider. All rights reserved.
 //
 
@@ -10,7 +10,19 @@ import SwiftUI
 
 struct AssignmentCalculator: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+             Color.black
+            .edgesIgnoringSafeArea(.all)
+            HStack{
+                Spacer()
+                Text("555")
+                .foregroundColor(.white)
+                .font(.system(size: 60))
+                .fontWeight(.light)
+                .padding()
+            }
+            
+        }
     }
 }
 
@@ -19,3 +31,32 @@ struct AssignmentCalculator_Previews: PreviewProvider {
         AssignmentCalculator()
     }
 }
+
+struct GrayButtonModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .foregroundColor(.white)
+            .font(.custom("", size: 50))
+            .padding(.horizontal, 50)
+            .padding(.vertical, 3)
+            .padding(.bottom, -19)
+            .background(Color.init( #colorLiteral(red: 0.5570569038, green: 0.5565273762, blue: 0.5783078074, alpha: 1)))
+            .clipShape(Capsule())
+
+    }
+}
+
+struct OrangeButtonModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .foregroundColor(.white)
+            .font(.custom("", size: 50))
+            .padding(.horizontal, 50)
+            .padding(.vertical, 3)
+            .padding(.bottom, -19)
+            .background(Color.init( #colorLiteral(red: 0.9990305305, green: 0.5842646956, blue: 0, alpha: 1)))
+            .clipShape(Capsule())
+
+    }
+}
+
